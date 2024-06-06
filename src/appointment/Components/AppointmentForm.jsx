060bee54155ff2ef619f066959d639de443391b7
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-06-06 14:40:14 +0300

    appointment app

--- src/appointment/Components/AppointmentForm.jsx
+++ src/appointment/Components/AppointmentForm.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "../Styles/AppointmentForm.css";
 import { ToastContainer, toast } from "react-toastify";
 
+
 function AppointmentForm() {
   const navigate = useNavigate();
 
@@ -84,85 +85,80 @@ function AppointmentForm() {
   };
 
   return (
-    <div className="appointment-form-section">
-      <h1 className="legal-siteTitle">
-        <Link to="/">
-          Health <span className="legal-siteSign">+</span>
-        </Link>
-      </h1>
+    <div className="">
+      <div className="p-2">
+        <h1 className="text-3xl text-blue-500 font-semibold">
+          Health <span className="navbar-sign">+</span>
+        </h1>
+      </div>
 
-      <div className="form-container">
-        <h2 className="form-title">
-          <span>Book Appointment Online</span>
+      <div className="form-container bg-blue-50 w-full flex flex-col">
+        <h2 className="text-2xl font-semibold">
+          <span>Book Appointment</span>
         </h2>
 
-        <form className="form-content" onSubmit={handleSubmit}>
-          <label>
-            Patient Full Name:
-            <input
-              type="text"
+
+        <form class=" mx-auto w-3/4 ">
+          <div class="mb-5">
+            <label for="patientName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient Name</label>
+            <input type="text"
               value={patientName}
               onChange={(e) => setPatientName(e.target.value)}
               required
-            />
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             {formErrors.patientName && (
               <p className="error-message">{formErrors.patientName}</p>
             )}
-          </label>
-
-          <br />
-          <label>
-            Patient Phone Number:
-            <input
-              type="text"
+          </div>
+          <div class="mb-5">
+            <label for="contact" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient phone number</label>
+            <input type="text"
               value={patientNumber}
               onChange={(e) => setPatientNumber(e.target.value)}
               required
-            />
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             {formErrors.patientNumber && (
               <p className="error-message">{formErrors.patientNumber}</p>
             )}
-          </label>
-
-          <br />
-          <label>
-            Patient Gender:
+          </div>
+          <div class="mb-5">
+            <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient gender</label>
             <select
               value={patientGender}
               onChange={(e) => setPatientGender(e.target.value)}
               required
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option value="default">Select</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
-              <option value="private">I will inform Doctor only</option>
+              <option value="private">I'd rather not say</option>
+              <option value="private">Custom</option>
             </select>
             {formErrors.patientGender && (
               <p className="error-message">{formErrors.patientGender}</p>
             )}
-          </label>
+          </div>
 
-          <br />
-          <label>
-            Preferred Appointment Time:
-            <input
-              type="datetime-local"
+          <div class="mb-5">
+            <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prefered appointment time</label>
+            <input type="datetime-local"
               value={appointmentTime}
               onChange={(e) => setAppointmentTime(e.target.value)}
               required
-            />
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             {formErrors.appointmentTime && (
               <p className="error-message">{formErrors.appointmentTime}</p>
             )}
-          </label>
+          </div>
 
-          <br />
-          <label>
-            Preferred Mode:
+          <div class="mb-5">
+            <label for="mode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prefered mode of appointment</label>
             <select
               value={preferredMode}
               onChange={(e) => setPreferredMode(e.target.value)}
               required
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option value="default">Select</option>
               <option value="voice">Voice Call</option>
@@ -171,21 +167,16 @@ function AppointmentForm() {
             {formErrors.preferredMode && (
               <p className="error-message">{formErrors.preferredMode}</p>
             )}
-          </label>
+          </div>
+            <div className="flex justify-center">
 
-          <br />
           <button type="submit" className="text-appointment-btn">
             Confirm Appointment
           </button>
-
-          <p
-            className="success-message"
-            style={{ display: isSubmitted ? "block" : "none" }}
-          >
-            Appointment details has been sent to the patients phone number via
-            SMS.
-          </p>
+            </div>
         </form>
+
+
       </div>
 
       <ToastContainer autoClose={5000} limit={1} closeButton={false} />
